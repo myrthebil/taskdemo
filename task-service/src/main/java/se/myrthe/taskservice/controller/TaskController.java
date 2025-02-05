9576ea807968dee0323e07ca569571a06ac2102c
Myrthe Bil <myrthebil@gmail.com>
2025-02-05 21:18:36 +0100

    [SPIC-7] create postmapping to collect tasks for a specific user, specified in the request body

--- task-service/src/main/java/se/myrthe/taskservice/controller/TaskController.java
+++ task-service/src/main/java/se/myrthe/taskservice/controller/TaskController.java
@@ -2,9 +2,11 @@ package se.myrthe.taskservice.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import se.myrthe.commonmodel.model.Task;
+import se.myrthe.commonmodel.model.User;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -14,9 +16,9 @@ public class TaskController {
 
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
-    @GetMapping
-    public Set<Task> getOwnedTasks(final String username) {
-        logger.info("Collecting tasks for user {}", username);
+    @PostMapping("/tasks")
+    public Set<Task> getOwnedTasks(@RequestBody final User user) {
+        logger.info("Collecting tasks for user {}", user.getUsername());
         return new HashSet<>();
     }
 }
