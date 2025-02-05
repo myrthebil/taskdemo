fd7c2e0648a6ebb8926fdc757482b0a1abf45237
Myrthe Bil <myrthebil@gmail.com>
2025-02-05 21:02:22 +0100

    [SPIC-7] add controller debugging logline and fix SpringApplication

--- task-service/src/main/java/se/myrthe/taskservice/TaskServiceApplication.java
+++ task-service/src/main/java/se/myrthe/taskservice/TaskServiceApplication.java
@@ -1,4 +1,12 @@
 package se.myrthe.taskservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class TaskServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskServiceApplication.class, args);
+    }
 }
--- task-service/src/main/java/se/myrthe/taskservice/controller/TaskController.java
+++ task-service/src/main/java/se/myrthe/taskservice/controller/TaskController.java
@@ -1,4 +1,22 @@
 package se.myrthe.taskservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.myrthe.commonmodel.model.Task;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
 public class TaskController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+
+    @GetMapping
+    public Set<Task> getOwnedTasks(final String username) {
+        logger.info("Collecting tasks for user {}", username);
+        return new HashSet<>();
+    }
 }
