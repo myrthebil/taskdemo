3cbf1edfce4230d29e6d6b9245c17606a67e0225
Myrthe Bil <myrthebil@gmail.com>
2025-02-05 13:30:53 +0100

    scaffolding: add content spring initializr

---
+++ src/test/java/se/myrthe/taskdemo/TaskdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package se.myrthe.taskdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
